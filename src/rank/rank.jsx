44f328682a17a0e201ca5b93aa905d549be12552
JacobMorrill030 <jmorrill509@lsr7.net>
2026-02-11 17:21:19 -0700

    reformatted rank page

--- src/rank/rank.jsx
+++ src/rank/rank.jsx
@@ -18,51 +18,56 @@ export function Rank() {
     </div>
     <br />
     <div className="container">
-        <div className="column">
-            <div className="list-container">
-                <ol className="ranked-list">
-                    <li className="ranked-item"><input type="text" value="item" readonly/></li>
-                    <li className="ranked-item"><input type="text" value="item" readonly/></li>
-                    <li className="ranked-item"><input type="text" value="item" readonly/></li>
-                    <li className="ranked-item"><input type="text" value="item" readonly/></li>
-                    <li className="ranked-item"><input type="text" value="item" readonly/></li>
-                </ol>
+        <div className="column1">
+            <div className="list_button">
+                <div className="list-container">
+                    <ol className="ranked-list">
+                        <div className="bank-container"><li><input className="list-item" type="text" value="item" readonly/></li></div>
+                        <div className="bank-container"><li><input className="list-item" type="text" value="item" readonly/></li></div>
+                        <div className="bank-container"><li><input className="list-item" type="text" value="item" readonly/></li></div>
+                        <div className="bank-container"><li><input className="list-item" type="text" value="item" readonly/></li></div>
+                        <div className="bank-container"><li><input className="list-item" type="text" value="item" readonly/></li></div>
+                    </ol>
+                </div>
+                <div className="item-container">
+                    <button className="item-btn">+ Add Item</button>
+                </div>
             </div>
-            <button className="item-btn">+ Add Item</button>
             <div className="drop-down">
                 <h3>Use Provided Rankings</h3>
-                <select className="drop-down-box">
-                    <option value="option1" selected>Superheroes</option>
-                    <option value="option2" selected>Fast Food Restaurants</option>
-                    <option value="option3" selected>Types of Chairs</option>
-                    <option value="option4" selected>Dinosaurs</option>
-                    <option value="option5" selected>Star Wars Characters</option>
-                </select>
+                <div class="drop-down-container">
+                    <select className="drop-down-box">
+                        <option value="option1" selected>Superheroes</option>
+                        <option value="option2" selected>Fast Food Restaurants</option>
+                        <option value="option3" selected>Types of Chairs</option>
+                        <option value="option4" selected>Dinosaurs</option>
+                        <option value="option5" selected>Star Wars Characters</option>
+                    </select>
+                </div>
             </div>
         </div>
-        <div className="column">
-            <br />
+        <div className="column2">
             <div className="table-container">
-                <table border="1" className="tier-list" cellpadding="40">
+                <table border="1" className="tier-list" cellpadding="30">
                     <tr>
                         <td className="s-tier">S</td>
-                        <td className="row"></td>
+                        <td className="t-row"></td>
                     </tr>
                     <tr>
                         <td className="a-tier">A</td>
-                        <td className="row"></td>
+                        <td className="t-row"></td>
                     </tr>
                     <tr>
                         <td className="b-tier">B</td>
-                        <td className="row"></td>
+                        <td className="t-row"></td>
                     </tr>
                     <tr>
                         <td className="c-tier">C</td>
-                        <td className="row"></td>
+                        <td className="t-row"></td>
                     </tr>
                     <tr>
                         <td className="d-tier">D</td>
-                        <td className="row"></td>
+                        <td className="t-row"></td>
                     </tr>
                 </table>
             </div>
@@ -70,11 +75,11 @@ export function Rank() {
             <fieldset className="item-bank">
                 <div>
                     <ul className="bank-list">
-                        <li><input className="list-item" type="text" value="item" readonly/></li>
-                        <li><input className="list-item" type="text" value="item" readonly/></li>
-                        <li><input className="list-item" type="text" value="item" readonly/></li>
-                        <li><input className="list-item" type="text" value="item" readonly/></li>
-                        <li><input className="list-item" type="text" value="item" readonly/></li>
+                        <div className="bank-container"><li><input className="list-item" type="text" value="item" readonly/></li></div>
+                        <div className="bank-container"><li><input className="list-item" type="text" value="item" readonly/></li></div>
+                        <div className="bank-container"><li><input className="list-item" type="text" value="item" readonly/></li></div>
+                        <div className="bank-container"><li><input className="list-item" type="text" value="item" readonly/></li></div>
+                        <div className="bank-container"><li><input className="list-item" type="text" value="item" readonly/></li></div>
                     </ul>
                 </div>
             </fieldset>
