710b480a52d248e92ed803bfe4b7f3bae2ad6fe9
JacobMorrill030 <jmorrill509@lsr7.net>
2026-02-19 18:11:00 -0700

    added features so createAccount behaves similarly to login
    some bugs I need to work out still

--- src/createAccount/createAccount.jsx
+++ src/createAccount/createAccount.jsx
@@ -1,26 +1,139 @@
 import React from 'react';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
 import '../styles/account.css';
 import '../styles/app.css';
 
-export function CreateAccount() {
+export function CreateAccount({ userName, authState, onAuthChange }) {
+  const [name, setName] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [confirm, setConfirm] = React.useState('');
+  const canSubmit =
+    name.trim() !== '' &&
+    password !== '' &&
+    password === confirm;
+
+  React.useEffect(() => {
+    if (authState === AuthState.Unauthenticated) {
+      setName('');
+      setPassword('');
+      setConfirm('');
+    }
+  }, [authState]);
+
   return (
     <main>
       <div className="welcome-container">
-        <h1>Create Account</h1>
+        {authState !== AuthState.Unknown && <h1>Create Account</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated
+            userName={userName}
+            onLogout={() => onAuthChange('', AuthState.Unauthenticated)}
+          />
+        )}
+
         <h2>Enter Login Information</h2>
-        <div>
-            <input type="login" placeholder="username" size="25"/>
-        </div>
-        <p></p>
-        <div>
-            <input type="password" placeholder="password" size="25"/>
-        </div>
-        <div>
-            <input type="password" placeholder="confirm password" size="25"/>
-        </div>
-        <br /> 
-        <NavLink to ="/rank" className="btn">Sign In</NavLink>
+
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+              userName={userName}
+              onLogin={(loginUserName) => {
+                  onAuthChange(loginUserName, AuthState.Authenticated);
+              }}
+          />
+        )}
+
+        {/* {authState === AuthState.Unauthenticated && ( */}
+          <>
+            <div className="login-container">
+              <input
+                type="login"
+                placeholder="username"
+                size="25"
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+            </div>
+            <div className="login-container">
+              <input
+                type="password"
+                placeholder="password"
+                size="25"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="login-container">
+              <input
+                type="password"
+                placeholder="confirm password"
+                size="25"
+                value={confirm}
+                onChange={e => setConfirm(e.target.value)}
+              />
+            </div>
+            <br />
+            <div className="button-containter">
+              <NavLink
+                to="/rank"
+                className="btn"
+                onClick={e => {
+                  if (!canSubmit) e.preventDefault();
+                  else onAuthChange(name, AuthState.Authenticated);
+                }}
+              >
+                Create
+              </NavLink>
+            </div>
+          </>
+        {/* // )} */}
+
+        {authState === AuthState.Unauthenticated && (
+          <>
+            <div className="login-container">
+              <input
+                type="login"
+                placeholder="username"
+                size="25"
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+            </div>
+            <div className="login-container">
+              <input
+                type="password"
+                placeholder="password"
+                size="25"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="login-container">
+              <input
+                type="password"
+                placeholder="confirm password"
+                size="25"
+                value={confirm}
+                onChange={e => setConfirm(e.target.value)}
+              />
+            </div>
+            <br />
+            <div className="button-containter">
+              <NavLink
+                to="/rank"
+                className="btn"
+                onClick={e => {
+                  if (!canSubmit) e.preventDefault();
+                  else onAuthChange(name, AuthState.Authenticated);
+                }}
+              >
+                Create
+              </NavLink>
+            </div>
+          </>
+        )}
       </div>
       <br />
       <br />
