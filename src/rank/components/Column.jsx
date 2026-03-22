b4410800841ca5f31513cb4fb953b384e4993c3c
JacobMorrill567 <jacobmorrill030@gmail.com>
2026-03-22 01:17:03 -0600

    Added functionality so the items in the ordered list and tier list are not connected, and you can sort the items within the tiers.
    Also added a landing placeholder for where it will land. Added sorting within the bank. There are some weird bugs with the bank, and in-tier sorting

--- src/rank/components/Column.jsx
+++ src/rank/components/Column.jsx
@@ -8,7 +8,7 @@ export const Column = ({tasks, onUpdateTask, onDeleteTask, onTypedChange}) => {
         <div className="column">
             <div className="list-container">
                 <ol className="ranked-list">
-                    <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
+                    <SortableContext items={tasks.map(task => task.id)} strategy={verticalListSortingStrategy}>
                         {tasks.map((task) => {
                             return (
                                 <Task
